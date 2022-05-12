78237c27e1e91d57a89efbbb22acb98b02c8d6c4
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-05-12 10:15:56 +0200

    solo queda el main

--- clases/grafico.py
+++ clases/grafico.py
@@ -8,6 +8,16 @@ class graficos:
         self.columnauno=columnauno
         self.columnados=columnados
     
-    def dibujargrafico(self, title):
+    def dibujargraficodeconversiones(self, title):
         self.dataset=fileuno
         self.dataset.groupby(self.columnauno)[self.columnados].sum().plot(tipo="bar")
+        plt.title(title)
+        plt.xlabel(None)
+        plt.show()
+    
+    def dibujargraficodenavegacion(self, title):
+        self.dataset=filedos
+        self.dataset.groupby(self.columnauno)[self.columnados].sum().plot(tipo="bar")
+        plt.title(title)
+        plt.xlabel(None)
+        plt.show()
