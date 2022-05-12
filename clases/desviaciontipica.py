011241954d8c0fca531cf1afe24243483820235c
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-05-12 09:10:53 +0200

    clase desviacion tipica

---
+++ clases/desviaciontipica.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class desviaciontipica:
+
+    def __init__(self, csv, columna):
+        self.csv=csv
+        self.columna=columna
+
+    def calculodesviaciontipica(self):
+        desviaciontipica=self.csv[self.columna].mad()
+        print("la desviacion tipica es: ", desviaciontipica)
