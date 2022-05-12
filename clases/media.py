a0333a051a69560d5baed2d526cab6183fd8bf2d
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-05-12 09:01:36 +0200

    importo los csv, y hago la clase media

---
+++ clases/media.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+class media:
+    def __init__(self, csv, columna):
+        self.columna=columna
+
+    def calcularmedia(self):
+        media=self.csv[self.columna].mean()
+        print("la media es: ", media)
